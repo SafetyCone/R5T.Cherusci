e5d87e01f3de6d6e9c7c2a05905527e984aeda0e
MinexAutomation <minex.automation@gmail.com>
2019-10-13 19:43:02 -0700

    Added initial file extensions.

--- source/R5T.Cherusci/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Cherusci
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Cherusci/Code/Classes/FileExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Cherusci
+{
+    public static class FileExtensions
+    {
+        public const string Binary = "bin";
+        public const string Data = "dat";
+        public const string Temp = "tmp";
+        public const string Text = "txt";
+    }
+}
