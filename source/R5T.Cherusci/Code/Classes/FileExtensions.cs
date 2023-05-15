85e0e894b2fec6ba92463cf7bd7b148623b87108
davidcoats <david@getsafetycone.com>
2023-05-14 23:44:08 -0700

    Interim changes.

--- source/R5T.Cherusci/Code/Classes/FileExtensions.cs
+++ source/R5T.Cherusci/Code/Classes/FileExtensions.cs
@@ -3,18 +3,35 @@
 
 namespace R5T.Cherusci
 {
+    [Obsolete("See R5T.Z0044.")]
     public static class FileExtensions
     {
+        [Obsolete("See R5T.Z0044.")]
         public const string Binary = "bin";
+
+        [Obsolete("See R5T.Z0044.")]
         public const string Data = "dat";
+
+        [Obsolete("See R5T.Z0044.")]
         public const string Json = "json";
+
+        [Obsolete("See R5T.Z0044.")]
         public const string Temporary = "tmp";
+
+        [Obsolete("See R5T.Z0044.")]
         public const string Text = "txt";
 
         // Images.
+        [Obsolete("See R5T.Z0044.")]
         public const string Bitmap = "bmp";
+
+        [Obsolete("See R5T.Z0044.")]
         public const string Jpeg = "jpeg";
+
+        [Obsolete("See R5T.Z0044.")]
         public const string Jpg = "jpg";
+
+        [Obsolete("See R5T.Z0044.")]
         public const string Png = "png";
     }
 }
---
+++ source/R5T.Cherusci/Project Plan.md
@@ -0,0 +1,7 @@
+# R5T.Cherusci - [Obsolete("See R5T.Z0044.")]
+A codenamed library of stringly-typed COMMON file-extension values.
+
+Related to R5T.Phillipi.
+
+Goals
+
